e84d6b306f22654671e34347f726031f65ad6edd
Андрей Хохлов <andrey@MacBook-Air-Andrej.local>
2024-09-19 22:32:41 +0300

    Add Component GameOld class

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import { GameLayout } from './game-layout';
 import { useDispatch } from 'react-redux';
 import { handlerReset } from './handlers';
+import { Component } from 'react';
 
 export const Game = () => {
 	const dispatch = useDispatch();
@@ -9,3 +10,21 @@ export const Game = () => {
 
 	return <GameLayout onClickReset={onClickReset} />;
 };
+
+export class GameOld extends Component {
+	onClickReset() {
+		handlerReset(dispatch);
+	}
+
+	render() {
+		return (
+			<div className={styles.game}>
+				<Information />
+				<Field />
+				<button className={styles['button-reset']} onClick={this.onClickReset}>
+					Начать заново
+				</button>
+			</div>
+		);
+	}
+}
